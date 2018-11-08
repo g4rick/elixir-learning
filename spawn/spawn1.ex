d070171133fa7e3ab39ad1a76a95b70b4324c5be
g4rick <285699093@qq.com>
2018-11-08 14:26:39 +0800

    add format

--- spawn/spawn1.ex
+++ spawn/spawn1.ex
@@ -2,15 +2,15 @@ defmodule Spawn1 do
   def greet do
     receive do
       {sender, msg} ->
-        send sender, { :ok, "Hello, #{msg}" }
+        send(sender, {:ok, "Hello, #{msg}"})
     end
   end
 end
 
 pid = spawn(Spawn1, :greet, [])
-send(pid, {self, "World!"})
+send(pid, {self(), "World!"})
 
 receive do
   {:ok, message} ->
-    IO.puts message
+    IO.puts(message)
 end
