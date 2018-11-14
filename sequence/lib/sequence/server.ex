9b681bc1626f3a6587dca5b5c57fe90d8ba17fbc
g4rick <285699093@qq.com>
2018-11-14 14:06:28 +0800

    format

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -13,10 +13,9 @@ defmodule Sequence.Server do
     GenServer.cast(__MODULE__, {:increment_number, delta})
   end
 
-
   # OTP-Servers-1
-  def handle_call(:pop, _from, [item|tail]) do
-    { :reply, item, tail }
+  def handle_call(:pop, _from, [item | tail]) do
+    {:reply, item, tail}
   end
 
   def handle_call(:next_number, _from, current_number) do
@@ -24,10 +23,10 @@ defmodule Sequence.Server do
   end
 
   def handle_cast({:increment_number, delta}, current_number) do
-    { :noreply, current_number + delta }
+    {:noreply, current_number + delta}
   end
 
-  def format_status(_reason, [ _pdict, state ]) do
-    [data: [{'State', "My current state is '#{inspect state}', and I`m happy"}]]
+  def format_status(_reason, [_pdict, state]) do
+    [data: [{'State', "My current state is '#{inspect(state)}', and I`m happy"}]]
   end
 end
